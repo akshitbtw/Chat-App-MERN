d32f4e3cd46565273860b2739353c3ae456f883c
Akshit <akshit.40@gmail.com>
2024-05-09 19:11:30 +0530

    minor css changes

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -26,15 +26,15 @@ const SignUp = () => {
 
   return (
     <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
-      <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+      <div className='w-full p-6 rounded-lg shadow-lg bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-2xl bg-opacity-0'>
         <h1 className='text-3xl font-semibold text-center text-gray-300'>
           SignUp
-          <span className='text-blue-500'> ChatApp</span>
+          <span className='text-yellow-300'> ChatApp</span>
         </h1>
 
         <form onSubmit={handleSubmit}>
           <div>
-            <label className='label p-2'>
+            <label className='label'>
               <span className='text-base label-text'>Full Name</span>
             </label>
             <input
@@ -49,7 +49,7 @@ const SignUp = () => {
           </div>
 
           <div>
-            <label className='label p-2'>
+            <label className='label'>
               <span className='text-base label-text'>Username</span>
             </label>
             <input
